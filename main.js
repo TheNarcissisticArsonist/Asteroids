0dc5c47c1dabe835049d491cc84fe3cd0ba203ee
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-20 10:13:00 -0400

    New Game makes game elements visibile.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var newGame = document.getElementById("new");
 var level = document.getElementById("currentLevel");
+var levelCont = document.getElementById("level");
 var score = document.getElementById("currentScore");
+var scoreCont = document.getElementById("score");
 var gameBoard = document.getElementById("gameBoard");
+
+newGame.addEventListener("click", function() {
+  levelCont.style.display = "inline-block";
+  scoreCont.style.display = "inline-block";
+  gameBoard.style.display = "inline-block";
+});
