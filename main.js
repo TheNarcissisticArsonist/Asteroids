feafa34ee6074b312d4f75043031f02a0d0ab68a
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-20 09:58:10 -0400

    Grab elements.

---
+++ main.js
@@ -0,0 +1,4 @@
+var newGame = document.getElementById("new");
+var level = document.getElementById("currentLevel");
+var score = document.getElementById("currentScore");
+var gameBoard = document.getElementById("gameBoard");
