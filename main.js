b25fd73059059cbf8f17f7e117b4cba32fe4613d
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-20 10:32:32 -0400

    Begin setting up level 1.

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ var score = document.getElementById("currentScore");
 var scoreCont = document.getElementById("score");
 var gameBoard = document.getElementById("gameBoard");
 
+function setUpLevel(numAsteroids) {
+  htmlString = "<img class=\"gameObject\" id=\"ship\" src=\"Spaceship.png\"></img>"
+  for(i=0; i<numAsteroids; ++i) {
+    htmlString += "<img class=\"gameObject bigAsteroid\" src=\"Asteroid.png\"></img>";
+  }
+  return htmlString;
+}
+
 newGame.addEventListener("click", function() {
   levelCont.style.display = "inline-block";
   scoreCont.style.display = "inline-block";
   gameBoard.style.display = "inline-block";
+  gameBoard.innerHTML = setUpLevel(3);
 });
