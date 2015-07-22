c89cf1b730df5895f016cacd4217a69ec585328c
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-22 11:11:21 -0400

    Grabbed elements to variables.

---
+++ main.js
@@ -0,0 +1,19 @@
+//Get user interface elements
+var newGameButton   = document.getElementById("new");
+var levelCont       = document.getElementById("levelCont");
+var levelDisplay    = document.getElementById("level");
+var scoreCont       = document.getElementById("scoreCont");
+var scoreDisplay    = document.getElementById("score");
+var lives           = document.getElementById("lives");
+
+//Get data display elements
+var dataPos         = document.getElementById("pos");
+var dataVel         = document.getElementById("vel");
+var dataAcl         = document.getElementById("acl");
+var dataVelM        = document.getElementById("velM");
+var dataAclM        = document.getElementById("aclM");
+var dataAngle       = document.getElementById("angle");
+var dataColliding   = document.getElementById("colliding");
+
+//Get game board element
+var gameBoard       = document.getElementById("gameBoard");
