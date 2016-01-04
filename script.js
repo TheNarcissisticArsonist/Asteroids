9d6fad1e6c3e3fa8f5c2d00dfc9c2903fba40bbc
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2016-01-04 14:09:17 -0500

    This may seem like weird formatting, but it collapses out of the way really nicely in atom.io

--- script.js
+++ script.js
@@ -10,14 +10,12 @@ boardWidth = 900;
 boardHeight = 600;
 boardBorderLineStyleString = "stroke: rgba(255,255,255,1);";
 
-var basicBoardOutlineSVG = "\
-<svg id='gameBoard' width='"+boardWidth+"' height='"+boardHeight+"'>\
+var basicBoardOutlineSVG = "<svg id='gameBoard' width='"+boardWidth+"' height='"+boardHeight+"'>\
   <line x1='0' y1='0' x2='"+boardWidth+"' y2='0' style='"+boardBorderLineStyleString+"'></line>\
   <line x1='"+boardWidth+"' y1='0' x2='"+boardWidth+"' y2='"+boardHeight+"' style='"+boardBorderLineStyleString+"'></line>\
   <line x1='"+boardWidth+"' y1='"+boardHeight+"' x2='0' y2='"+boardHeight+"' style='"+boardBorderLineStyleString+"'></line>\
   <line x1='0' y1='"+boardHeight+"' x2='0' y2='0' style='"+boardBorderLineStyleString+"'></line>\
-</svg>\
-"
+  </svg>"
 
 //Generate the game board
 htmlELEMENTS.gameBoardCont.innerHTML = "";
